40a9bd312d74fb2dbe4f9157b2e0a49571f65c12
RENATO TEIXEIRA LIMA <renatolimaster@gmail.com>
2019-12-25 12:07:01 -0300

    Update environment.prod.ts

--- Secretary.SPA/src/environments/environment.prod.ts
+++ Secretary.SPA/src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  APIUrlBase: 'http://api.ipapi.com/',
-  APIAccessKey: '?access_key=b6d41a704a979e7a1649e770af119529',
-  googleApiKey: 'AIzaSyBQAV_Z2e5TYYu5Ev5tRrtTrHq5tKyJMkk',
   production: true,
   apiUrl: 'http://localhost:5000/api/'
 };

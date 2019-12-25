d16bb75d88d1ca4c3fad5caa3de6a6e09941ff61
Renato Lima <renatolimaster@gmail.com>
2019-12-25 11:55:37 -0300

    after environments

--- Secretary.SPA/src/environments/environment.ts
+++ Secretary.SPA/src/environments/environment.ts
@@ -3,10 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  APIUrlBase: 'http://api.ipapi.com/',
-  APIAccessKey: '?access_key=b6d41a704a979e7a1649e770af119529',
-  googleApiKey: 'AIzaSyBQAV_Z2e5TYYu5Ev5tRrtTrHq5tKyJMkk',
-  production: false,
+ 
   apiUrl: 'http://localhost:5000/api/'
 };
 

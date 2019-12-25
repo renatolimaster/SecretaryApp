f585332bdb1c1f8824844794d318bb66e5b8e588
Renato Lima <renatolimaster@gmail.com>
2019-12-25 11:59:39 -0300

    environments ignore

--- Secretary.SPA/src/environments/environment.prod.ts
+++ Secretary.SPA/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
-  
+  APIUrlBase: 'http://api.ipapi.com/',
+  APIAccessKey: '?access_key=b6d41a704a979e7a1649e770af119529',
+  googleApiKey: 'AIzaSyBQAV_Z2e5TYYu5Ev5tRrtTrHq5tKyJMkk',
+  production: true,
   apiUrl: 'http://localhost:5000/api/'
 };
